de03f4115c742ae46723701f31fde90f968ee007
cmathis <cmathis@gopro.com>
2017-04-21 13:18:43 +0200

    remove un-needed print

--- pizzaOrganizer.py
+++ pizzaOrganizer.py
@@ -24,7 +24,6 @@ SLACK_CLIENT ,BOT_ID ,AT_BOT, AT_CHAN = get_slackConstants(SLACKBOT_TOKEN, "pizz
 def parseChoices():
     choices = [ ]
     inputFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),"pizzaChoices.txt")
-    print inputFile
     f = open(inputFile,'r')
     lines = f.readlines()
     f.close()
